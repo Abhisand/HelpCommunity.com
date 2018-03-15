76964ba727b3834a7a29e27c1b42b119d94643e7
Abhishek <abhisand100@gmail.com>
2018-03-15 07:42:59 +0530

    Template concept in python3

---
+++ Alieans/__init__.py
@@ -0,0 +1,18 @@
+from string import Template
+
+def Main():
+    cart = []
+    cart.append(dict(item="Coke",Prize=15,qty=2))
+    cart.append(dict(item="Sprite", Prize=10, qty=1))
+    cart.append(dict(item="Maza", Prize=20, qty=5))
+
+    t = Template("$qty x $item = $Prize")
+    total = 0
+    print("Cart:")
+    for data in cart :
+        print(t.substitute(data))
+        total+=data["Prize"]
+    print("Total :"+str(total))
+
+if __name__ == "__main__":
+    Main()
